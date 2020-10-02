a2e49d314de6f656fc33f7282fa2d80b9a989862
Alexander Pagliaro <pagliaro.a@gmail.com>
2020-10-01 23:58:39 -0400

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
     targets: [
         .target(
             name: "Armchair",
-            path: "./Source"
+            path: "./Source",
+            publicHeadersPath: "./Source/Armchair.h"
         )
     ]
     
